4e47938604304c86c6f22db2bbce00777e518d75
Anthony MARQUES <anthony.marqu3s@gmail.com>
2021-04-09 11:20:31 +0200

    Template vue+dfn

---
+++ README.md
@@ -0,0 +1,14 @@
+# DFINITY-Vue
+https://sdk.dfinity.org/docs/developers-guide/tutorials/explore-templates.html
+
+```
+DFX_VERSION=0.6.26 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
+dfx start
+```
+```
+dfx deploy
+ou
+dfx canister create --all
+dfx build
+dfx canister install --all -m reinstall
+```
---
+++ src/frontend/public/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <link
+      href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css"
+      rel="stylesheet"
+    />
+    <v-container>
+    <p>{{ greetings }}</p>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import auth from "ic:canisters/auth";
+
+export default {
+  data: () => {
+    return {
+      greetings: "",
+    }
+  },
+
+  created() {
+    auth.greet().then((greetings) => {
+      this.greetings = greetings
+    });
+  },
+  methods: {},
+};
+</script>
